44b856dcc85ce3cc9b4c527ad192d94b75053c41
gareWare <92345400+gare-Ware@users.noreply.github.com>
2022-04-04 16:51:24 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import quizzicalHome from './imgs/quizzical-home.png'
+import quizzicalGameplay from './imgs/quizzical-gameplay.png'
+import snakeDesktop from './imgs/snake-gameplay.png'
+import snakeMobile from './imgs/snake-mobile.png'
+import tenzies from './imgs/tenzies.png'
+import speedTyping from './imgs/speed-typing.png'
+import portfolioDesktop from "./imgs/portfolio-desktop.png"
+import portfolioMobile from "./imgs/portfolio-mobile.png"
+import abzijgenDesktop from './imgs/abzijgen-desktop.png'
+import abzijgenMobile from './imgs/abzijgen-mobile.png'
+
+
+const quizzicalImgs = [quizzicalHome, quizzicalGameplay]
+const snakeImgs = [snakeDesktop, snakeMobile]
+const abzijgenImgs = [abzijgenDesktop, abzijgenMobile]
 
 function App() {
+
+  const displayImgs = arr => arr.map(img => <img src={img}/>)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {displayImgs(abzijgenImgs)}
     </div>
   );
 }
